44320b3cd6a927b93f8fcaa1599753529149383c
cogear <cogear@qq.com>
2024-02-20 14:59:42 +0800

    TheadPool step 2, implement ThreadPool.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,45 @@
+use std::sync::{Arc, mpsc, Mutex};
+use std::sync::mpsc::Receiver;
 use std::thread;
 use std::thread::JoinHandle;
 
-pub struct ThreadPool;
+pub struct ThreadPool{
+    workers: Vec<JoinHandle<()>>,
+    sender: mpsc::Sender<Job>,
+}
 
-impl ThreadPool {
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool
+{
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        ThreadPool
+        let mut workers = Vec::with_capacity(size);
+        let (tx, rx) = mpsc::channel();
+        let receiver: Arc<Mutex<Receiver<Job>>> = Arc::new(Mutex::new(rx));
+
+        for x in 0..size {
+            let receiver_ref = Arc::clone(&receiver);
+            let worker = thread::spawn(move || loop {
+                let job = receiver_ref.lock().unwrap().recv().unwrap();
+
+                job();
+                println!("receive a job");
+            });
+            workers.push(worker);
+        }
+
+        ThreadPool {
+            sender: tx,
+            workers
+        }
     }
 
-    pub fn execute<F>(&self, f: F) -> JoinHandle<()>
+    pub fn execute<F>(&self, f: F)
     where F: FnOnce() + Send + 'static
     {
-        thread::spawn(|| {
-            f();
-        })
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
